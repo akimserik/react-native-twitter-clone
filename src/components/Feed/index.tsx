c0d7d0e3dd1d52eb6f9ea1a1e16b6100742f0fd1
Serik <serikakimgereyev@tengizchevroil.com>
2022-08-25 11:22:05 +0600

    add-loader-tweets-feed

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,29 +1,43 @@
-import React, {useEffect, useState} from "react";
-import { FlatList, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, View } from "react-native";
 import tweets from "../../data/tweets";
 import TweetComponent from "../Tweet";
-
-import { DataStore } from '@aws-amplify/datastore';
-import { Tweet } from '../../../src/models';
+import { DataStore } from "@aws-amplify/datastore";
+import { Tweet } from "../../../src/models";
+import styles from "./style";
 
 const Feed = () => {
-  const [tweets, setTweets] = useState<Tweet[]>([])
+  const [tweets, setTweets] = useState<Tweet[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   const getTweets = async () => {
+    setIsLoading(true);
     const models = await DataStore.query(Tweet);
-    setTweets(models)
-  }
+    console.log(models);
+    setTweets(models);
+    setIsLoading(false);
+  };
   useEffect(() => {
     getTweets();
-  },[])
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={[styles.container, styles.horizontal]}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
 
   return (
-  <View style={{ width: "100%" }}>
-    <FlatList
-      data={tweets}
-      keyExtractor={item => item.id}
-      renderItem={({ item }) => <TweetComponent tweet={item} />}
-    />
-  </View>
-)};
+    <View style={{ width: "100%" }}>
+      <FlatList
+        data={tweets}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <TweetComponent tweet={item} />}
+      />
+    </View>
+  );
+};
 
 export default Feed;
