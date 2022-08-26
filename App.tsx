8c21a910e39b848e8340669c7db8bf4b251a29f1
Serik <serikakimgereyev@tengizchevroil.com>
2022-08-26 13:11:21 +0600

    added-authenticator

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import useColorScheme from "./src/hooks/useColorScheme";
 import Navigation from "./src/navigation";
 
 import { Amplify } from "aws-amplify";
-import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Authenticator } from "@aws-amplify/ui-react";
 import awsconfig from "./src/aws-exports";
 // import "@aws-amplify/ui-react/Styles.css";
 import React from "react";
@@ -17,12 +17,18 @@ function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
-  return (
-    <SafeAreaProvider>
-      <Navigation colorScheme={colorScheme} />
-      <StatusBar />
-    </SafeAreaProvider>
-  );
+  if (!isLoadingComplete) {
+    return <></>;
+  } else {
+    return (
+      <Authenticator>
+        <SafeAreaProvider>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </SafeAreaProvider>
+      </Authenticator>
+    );
+  }
 }
 
-export default withAuthenticator(App);
+export default App;
